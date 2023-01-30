1eb6b2c5959ddf1dad3ae6ccb0a7329ea8008ff5
Eugene M <eugene.moschyn@gmail.com>
2023-01-29 19:01:13 -0800

    add(): mikro-orm config loader function

---
+++ src/common/config/database.config.ts
@@ -0,0 +1,22 @@
+import { ConfigModule, ConfigService } from "@nestjs/config";
+import { MikroOrmModuleAsyncOptions } from "@mikro-orm/nestjs";
+
+
+export function get_db_config(): MikroOrmModuleAsyncOptions
+{
+    return {
+        useFactory: (config: ConfigService) =>
+        ({
+            type:      'postgresql',
+            host:       config.get('DB_HOST'),
+            port:       config.get('DB_PORT'),
+            user:       config.get('DB_USERNAME'),
+            password:   config.get('DB_PASSWORD'),
+            dbName:     config.get('DB_NAME'),
+            entities:   ['dist/**/*.entity.ts'],
+            entitiesTs: ['src/**/*.entity.ts']
+        }),
+        imports: [ConfigModule],
+        inject:  [ConfigService]
+    }
+}
