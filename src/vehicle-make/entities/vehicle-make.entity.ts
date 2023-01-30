daae94ee21a4f93123135d61ac752c4c884a9355
Eugene M <eugene.moschyn@gmail.com>
2023-01-29 19:28:47 -0800

    add(): basic vehicle-make structure

--- src/vehicle-make/entities/vehicle-make.entity.ts
+++ src/vehicle-make/entities/vehicle-make.entity.ts
@@ -1,1 +1,12 @@
-export class VehicleMake {}
+import { Entity, PrimaryKey, Property } from "@mikro-orm/core";
+
+
+@Entity()
+export class VehicleMake
+{
+    @PrimaryKey()
+    make_id: number;
+
+    @Property()
+    make_name: string;
+}
