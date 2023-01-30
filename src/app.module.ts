ce4b317e1bf3e4b7eb5d4b400ad3e19fa76fdca6
Eugene M <eugene.moschyn@gmail.com>
2023-01-29 20:32:13 -0800

    add(): validation schema and mikro orm module init

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,16 @@
-import { ConfigModule } from '@nestjs/config';
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MikroOrmModule } from '@mikro-orm/nestjs';
 
+import { get_db_config } from '@/config';
+import { validationSchema } from '@/validation';
 import { VehicleMakeModule } from './vehicle-make/vehicle-make.module';
 
 
 @Module({
     imports: [
-        ConfigModule.forRoot(),
+        ConfigModule.forRoot({ validationSchema }),
+        MikroOrmModule.forRootAsync( get_db_config() ),
 
         VehicleMakeModule
     ],
