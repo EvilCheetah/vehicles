2d2ee12cc277ca85319973af4f14697f9ba9029d
Eugene M <eugene.moschyn@gmail.com>
2023-01-29 23:13:36 -0800

    add(): basic crud

--- src/vehicle-make/vehicle-make.service.ts
+++ src/vehicle-make/vehicle-make.service.ts
@@ -1,37 +1,62 @@
-import { Injectable } from '@nestjs/common';
-import { CreateVehicleMakeDto } from './dto/create-vehicle-make.dto';
-import { UpdateVehicleMakeDto } from './dto/update-vehicle-make.dto';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { EntityRepository, wrap } from '@mikro-orm/core';
+import { InjectRepository } from '@mikro-orm/nestjs';
+
+import { VehicleMake } from './entities/vehicle-make.entity';
+import { CreateVehicleMakeDTO } from './dto/create-vehicle-make.dto';
+import { UpdateVehicleMakeDTO } from './dto/update-vehicle-make.dto';
 
 
 @Injectable()
 export class VehicleMakeService
 {
-    create(createVehicleMakeDto: CreateVehicleMakeDto)
+    constructor(
+        @InjectRepository(VehicleMake)
+        private readonly makeRepository: EntityRepository<VehicleMake>
+    ) {}
+
+    async create(createVehicleMakeDTO: CreateVehicleMakeDTO)
     {
-        return 'This action adds a new vehicleMake';
+        const make = this.makeRepository.create(createVehicleMakeDTO);
+        await this.makeRepository.persistAndFlush(make);
+
+        return make;
     }
 
 
     findAll()
     {
-        return `This action returns all vehicleMake`;
+        return this.makeRepository.findAll();
     }
 
 
-    findOne(id: number)
+    async findOne(make_id: number)
     {
-        return `This action returns a #${id} vehicleMake`;
+        const make = await this.makeRepository.findOne({ make_id });
+
+        if ( !make )
+            throw new NotFoundException(`Vehicle make with id '${make_id}' was NOT FOUND`);
+        
+        return make;
     }
 
 
-    update(id: number, updateVehicleMakeDto: UpdateVehicleMakeDto)
+    async update(make_id: number, updateVehicleMakeDTO: UpdateVehicleMakeDTO)
     {
-        return `This action updates a #${id} vehicleMake`;
+        const make = await this.findOne(make_id);
+
+        wrap(make).assign(updateVehicleMakeDTO);
+        await this.makeRepository.persistAndFlush(make);
+
+        return make;
     }
 
     
-    remove(id: number)
+    async remove(make_id: number)
     {
-        return `This action removes a #${id} vehicleMake`;
+        const make = await this.findOne(make_id);
+        await this.makeRepository.removeAndFlush(make);
+
+        return make;
     }
 }
