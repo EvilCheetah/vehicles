2d2b2677d1d7361aca392234d74d1997888b6bf0
Eugene M <eugene.moschyn@gmail.com>
2023-01-29 23:12:58 -0800

    add(): repository necessary imports

--- src/vehicle-make/vehicle-make.module.ts
+++ src/vehicle-make/vehicle-make.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { VehicleMakeService } from './vehicle-make.service';
 import { VehicleMakeController } from './vehicle-make.controller';
+import { MikroOrmModule } from '@mikro-orm/nestjs';
+import { VehicleMake } from './entities/vehicle-make.entity';
 
 
 @Module({
+    imports:     [MikroOrmModule.forFeature([ VehicleMake ])],
     controllers: [VehicleMakeController],
     providers:   [VehicleMakeService]
 })
